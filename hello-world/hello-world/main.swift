3a643fa0e673416fc0f0511490f0ba87040789a2
Ievgen Gavrysh <zhenya.ga@gmail.com>
2019-07-19 11:28:29 -0700

    Add hello-world test task

---
+++ hello-world/hello-world/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  hello-world
+//
+//  Created by Ievgen Gavrysh on 7/19/19.
+//  Copyright © 2019 Ievgen Gavrysh. All rights reserved.
+//
+
+import Foundation
+
+func helloWorldText() -> String {
+  return "Hello, World!"
+}
+
+print(helloWorldText())
+
