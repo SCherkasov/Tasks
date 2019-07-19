2de1753d61d545638ad10592928d625d3919022e
Stasik <scherkasov8@gmail.com>
2019-07-19 20:41:05 +0300

    Task_3

--- Task(1)/main.swift
+++ Task(1)/main.swift
@@ -9,15 +9,23 @@
 import Foundation
 
 /*
- Напишите программу, которая позволяет пользователю ввести в консоли латинскую
- букву нижнего регистра,  переводит её в верхний регистр и результат выводит
- в консоль.
-*/
+ Составить программу, которая требует ввести два числа. Если первое число
+ больше второго, то программа печатает слово больше. Если первое число меньше
+ второго, то программа печатает слово меньше. А если числа равны, программа
+ напечатает сообщение Эти числа равны.
+ */
 
-func getHighRegister(letter: String) -> String {
-  
-  return letter.uppercased()
-}
+func compareNumbers(num1: Int, num2: Int) {
+  if num1 > num2 {
+    print("Bigger")
+  }
+    if num1 < num2 {
+      print("Less then")
+  }
+      if num1 == num2 {
+        print("numbers are equal")
+      }
+    }
 
-let x = getHighRegister(letter: "g")
+let x = compareNumbers(num1: 0, num2: 1)
 print(x)
